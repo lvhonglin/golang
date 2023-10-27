9bed4f3d255f84fadc489f4c90ac2f3be5007b95
lucyhllv <lucylv@21kunpeng.com>
2023-10-27 18:11:42 +0800

    cobra

--- com/lucy/test/study/flag/cobra/cmd/times.go
+++ com/lucy/test/study/flag/cobra/cmd/times.go
@@ -1,26 +1,43 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
 	"strings"
 )
 
 var echoTimes int
+var kaiguan bool
 var cmdTimes = &cobra.Command{
 	Use:   "time [string to time]",
 	Short: "Echo anything to the screen more times",
 	Long:  (`echo things multiple times back to the user y providing a count and a string`),
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		for i := 0; i < echoTimes; i++ {
-			fmt.Println("Echo: " + strings.Join(args, " "))
+		if kaiguan {
+			for i := 0; i < echoTimes; i++ {
+				fmt.Println("Echo: " + strings.Join(args, " "))
+			}
 		}
+	},
+	PreRun: func(cmd *cobra.Command, args []string) {
+		println("前置执行")
+	},
+	//postRunE和postRun只能写一个，不能写2个，写2个其中一个会被覆盖
+	PostRunE: func(cmd *cobra.Command, args []string) error {
+
+		fmt.Println("后置处理")
+		if strings.Contains(args[0], "err") {
+			return errors.New("字符里面有敏感信息")
+		}
+		return nil
 	},
 }
 
 func init() {
 	cmdTimes.Flags().IntVarP(&echoTimes, "times", "t", 1, "times to echo the input")
+	cmdTimes.Flags().BoolVarP(&kaiguan, "kaiguan", "k", false, "zhege shi kaiguan")
 	//times是echo的子目录
 	cmdEcho.AddCommand(cmdTimes)
 }
--- com/lucy/test/study/flag/cobra/main/main.go
+++ com/lucy/test/study/flag/cobra/main/main.go
@@ -3,4 +3,6 @@ package main
 func main() {
 	//main方法在项目根目录
 	//用go build -o hello，打包成hello
+	//打包之后执行：.\hello.exe echo time helloworld da -t 2 -k true
+	//打包之后执行：.\hello.exe echo time helloworld da -t 2 --kaiguan
 }
